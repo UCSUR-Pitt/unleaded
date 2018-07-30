c42e08ef84a9f620a55d5631c34b3f3ea6be7233
drw <drw@users.noreply.github.com>
2018-07-30 11:07:25 -0400

    Add support for POST requests as well as GET requests

--- views.py
+++ views.py
@@ -3,16 +3,27 @@ from django.http import HttpResponse, JsonResponse
 from .models import PipeRecord
 
 def handle_submission(request):
-    """Accept a GET request, parse the keys, and file the contents away in the database."""
+    """Accept a GET or POST request, parse the keys, and file the contents away in the database."""
     # To increase robustness, use the following to get all model keys
         # myproject.myapp.models.MyModel._meta.get_all_field_names()
     # then strip out created_at and iterate over the results.
-    home = request.GET['home']
+    if request.method == 'GET':
+        qd = request.GET
+    elif request.method == 'POST':
+        qd = request.POST
+    else:
+        response_data = {"success": False,
+                "message": "Unable to parse a {} request.".format(request.method)
+        }
+        return JsonResponse(response_data)
+
+    home = qd['home']
+
     steps_dict = {}
     for k in range(1,7):
         step_k = 'step{}'.format(k)
-        if step_k in request.GET:
-            steps_dict[step_k] = request.GET[step_k]
+        if step_k in qd:
+            steps_dict[step_k] = qd[step_k]
 
     pr = PipeRecord(home=home, **steps_dict)
     pr.save()
@@ -28,4 +39,4 @@ def handle_submission(request):
     return JsonResponse(response_data)
 
 def index(request):
-    return HttpResponse("Hello, Zaphod. You're at the pipe_data index.")
+    return HttpResponse("Welcome to the index!")
