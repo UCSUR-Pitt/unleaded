71d8d6da89bbfe69241dd6c4ee26c2f83ca7ee69
drw <drw@users.noreply.github.com>
2018-07-31 13:49:48 -0400

    Add admin-view name for PipeRecords to 'submissions'

--- models.py
+++ models.py
@@ -13,8 +13,7 @@ class PipeRecord(models.Model):
     step6 = models.CharField(max_length=100)
 
     class Meta:
-        verbose_name = "record of user answers"
-        verbose_name_plural = "records of user answers"
+        verbose_name = "submission"
 
     def __str__(self):
         return '{} ({}, {}, {}, {}, {}, {}, {})'.format(self.created_at, self.home, self.step1, self.step2, self.step3, self.step4, self.step5, self.step6)
